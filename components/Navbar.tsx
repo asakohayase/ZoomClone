a82762899f17e3cdedd3012c8c7c886ea2878dff
Asako Hayase <chipmunk.kck@gmail.com>
2024-04-02 16:19:08 -0700

    update: add mobile nav and fix logic for active route

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import MobileNav from "./MobileNav";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="fixed z-50 flex w-full justify-between bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          alt="Yoom logo"
+          width={32}
+          height={32}
+          className="max-sm:size-10"
+        ></Image>
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Yoom
+        </p>
+      </Link>
+      <div className="flex justify-between gap-5">
+        {/* clerk implementation */}
+        <MobileNav />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -14,7 +14,7 @@ const Sidebar = () => {
       <div className="flex flex-1 flex-col gap-6">
         {sidebarLinks.map((link) => {
           const isActive =
-            pathName === link.route || pathName.startsWith(link.route);
+            pathName === link.route || pathName.startsWith(`${link.route}/`);
           return (
             <Link
               href={link.route}
