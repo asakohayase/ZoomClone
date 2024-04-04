0b240ade645bd3f45847e174ac4f626744279093
Asako Hayase <chipmunk.kck@gmail.com>
2024-04-04 14:45:36 -0700

    update: create stream video provider

---
+++ actions/stream.actions.ts
@@ -0,0 +1,21 @@
+"use server";
+
+import { StreamClient } from "@stream-io/node-sdk";
+import { currentUser } from "@clerk/nextjs";
+
+const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+const apiSecret = process.env.STREAM_SECRET_KEY;
+
+export const tokenProvider = async () => {
+  const user = await currentUser();
+  if (!user) throw new Error("User is not authenticated");
+  if (!apiKey) throw new Error("API key is missing");
+  if (!apiSecret) throw new Error("API secret is missing");
+
+  const client = new StreamClient(apiKey, apiSecret);
+  const exp = Math.round(new Date().getTime() / 1000) + 60 * 60;
+  const issued = Math.floor(Date.now() / 1000) - 60;
+  const token = client.createToken(user.id, exp, issued);
+
+  return token;
+};
