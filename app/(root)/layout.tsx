7c954f019024ea2eef777ce745d35d4ddc31be00
Asako Hayase <chipmunk.kck@gmail.com>
2024-04-05 23:13:09 -0700

    update: create meeting room

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,13 @@
 import { ReactNode } from "react";
 
 import StreamVideoProvider from "@/providers/StreamClientProvider";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Yoom",
+  description: "Video conferencing for the modern age",
+  icons: { icon: "/icons/logo.svg" },
+};
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,5 +1,100 @@
+import { useState } from "react";
+
+import { cn } from "@/lib/utils";
+import { CallLayoutType } from "@/types";
+import {
+  CallControls,
+  CallParticipantsList,
+  CallStatsButton,
+  CallingState,
+  PaginatedGridLayout,
+  SpeakerLayout,
+  useCallStateHooks,
+} from "@stream-io/video-react-sdk";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LayoutList, Users } from "lucide-react";
+import { useSearchParams } from "next/navigation";
+import EndCallButton from "./EndCallButton";
+import Loader from "./Loader";
+
 const MeetingRoom = () => {
-  return <div>MeetingRoom</div>;
+  const searchParams = useSearchParams();
+  const isPersonalRoom = !!searchParams.get("personal");
+  const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
+  const [showParticipants, setShowParticipants] = useState(false);
+  const { useCallCallingState } = useCallStateHooks();
+
+  const callingState = useCallCallingState();
+
+  if (callingState !== CallingState.JOINED) return <Loader />;
+
+  const CallLayout = () => {
+    switch (layout) {
+      case "grid":
+        return <PaginatedGridLayout />;
+      case "speaker-right":
+        return <SpeakerLayout participantsBarPosition="left" />;
+      default:
+        return <SpeakerLayout participantsBarPosition="right" />;
+    }
+  };
+
+  return (
+    <section className="relative h-screen w-full overflow-hidden pt-4">
+      <div className="relative flex size-full items-center justify-center">
+        <div className="flex size-full max-w-[1000px] items-center">
+          <CallLayout />
+        </div>
+        <div
+          className={cn("ml-2 hidden h-[calc(100vh-86px)]", {
+            "show-block": showParticipants,
+          })}
+        >
+          <CallParticipantsList onClose={() => setShowParticipants(false)} />
+        </div>
+      </div>
+      <div className="fixed bottom-0 flex w-full flex-wrap items-center justify-center gap-5">
+        <CallControls />
+        <DropdownMenu>
+          <div className="flex items-center">
+            <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
+              <LayoutList size={20} className="text-white" />
+            </DropdownMenuTrigger>
+          </div>
+          <DropdownMenuContent className="border-dark-1 bg-dark-1 text-white">
+            {["Grid", "Speaker-Left", "Speaker-Right"].map((item) => (
+              <div key={item}>
+                <DropdownMenuItem
+                  className="cursor-pointer"
+                  onClick={() =>
+                    setLayout(item.toLowerCase() as CallLayoutType)
+                  }
+                >
+                  {item}
+                </DropdownMenuItem>
+              </div>
+            ))}
+            <DropdownMenuSeparator className="border-dark-1" />
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <CallStatsButton />
+        <button
+          onClick={() => setShowParticipants((prev) => !prev)}
+          className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]"
+        >
+          <Users size={20} className="text-white" />
+        </button>
+        {!isPersonalRoom && <EndCallButton />}
+      </div>
+    </section>
+  );
 };
 
 export default MeetingRoom;
